d43c3fefbe24296c7137c23c51acb20879b612dd
omj9803 <myeongju0316@gmail.com>
2022-04-04 17:58:12 +0900

    [2480]주사위 세개

--- BOJ/boj_2480.py
+++ BOJ/boj_2480.py
@@ -3,19 +3,19 @@ import sys
 a, b, c = map(int, sys.stdin.readline().split(' '))
 
 if a > b:
-    if a > c:
+    if a == c or b == c:
+        print(1000 + c * 100)
+    elif a > c:
         print(a * 100)
     elif a < c:
         print(c * 100)
-    else:
-        print(1000 + a * 100)
 elif a < b:
-    if b > c:
+    if a == c or b == c:
+        print(1000 + c * 100)
+    elif b > c:
         print(b * 100)
     elif b < c:
         print(c * 100)
-    else:
-        print(1000 + b * 100)
 elif a == b:
     if b > c or b < c:
         print(1000 + a * 100)
